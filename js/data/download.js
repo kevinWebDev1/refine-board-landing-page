7a6ea8c1ba9b7ee40ab8766230a0ff90c47118c2
kevinWebDev1 <kevinbusiness62@gmail.com>
2025-11-19 12:29:26 +0530

    Exposing downloadApp globally for inline HTML use

--- js/data/download.js
+++ js/data/download.js
@@ -2,3 +2,6 @@ export default function downloadApp() {
     const appUrl = "https://github.com/kevinWebDev1/refine-board-landing-page/releases/latest/download/refiner-keyboard.apk";
     window.location.href = appUrl;
 }
+
+// Exposing downloadApp globally for inline HTML use
+window.downloadApp = downloadApp;
