df1037a5265141fd544273c223b42fab24582869
kevinWebDev1 <kevinbusiness62@gmail.com>
2025-11-19 12:19:51 +0530

    fixed download.js

--- js/data/download.js
+++ js/data/download.js
@@ -1,5 +1,5 @@
 export default function downloadApp() {
-    // Replace with your real app URL
     const appUrl = "https://github.com/kevinWebDev1/refine-board-landing-page/releases/latest/download/refiner-keyboard.apk";
+    return appUrl;
 }
 
